472ca6f0d775aeda4778bb2501be4ec87796000f
Ryan Michael <ryan.michael.tech@gmail.com>
2025-01-20 15:19:01 -0500

    pi deployment fixes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # see https://stackoverflow.com/questions/44376846/creating-a-password-in-bash
 # create postgres password
-(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 20) > postgres_pw.txt
+(tr -dc 'A-Za-z0-9=' < /dev/urandom | head -c 20) > postgres_pw.txt
 # create session encryption key
-(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 100) > encryption_key.txt
+(tr -dc 'A-Za-z0-9=' < /dev/urandom | head -c 100) > encryption_key.txt
 docker compose down
 docker compose up -d
